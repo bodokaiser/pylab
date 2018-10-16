3f709256fc89def0af2d8fe1de947d512a3e991a
Bodo Kaiser <i@bodokaiser.io>
2018-10-16 14:44:58 +0200

    improved rigol measurement methods

--- pylab/scope/rigol.py
+++ pylab/scope/rigol.py
@@ -41,11 +41,36 @@ class DS1000(Device):
     """
     return self.resource.write(':KEY:LOCK DISable')
 
-  def measure(self, param, channel=1):
+  def measure(self, param, channel):
     """
-    Returns the measurement value of the specified parameter. Param can be
-    vpp, vavg, vmin, ..., etc.
+    Returns the measurement value of the specified parameter.
     """
-    param = param.upper()
+    self.resource.write(f':MEASure:{param}? CHANnel{channel}')[0]
 
-    return self.resource.query_ascii_values(f':MEASure:{param} CHANnel{channel}')[0]
+    return float(self.resource.read())
+    # using this will let the connection die very fast...
+    # return self.resource.query_ascii_values(f':MEASure:{param}? CHANnel{channel}')[0]
+
+  def measure_mean_voltage(self, channel=1):
+    """
+    Returns the average voltage measured.
+    """
+    return self.measure('VAVerage', channel)
+
+  def measure_rms_voltage(self, channel=1):
+    """
+    Returns the root means squared voltage measured.
+    """
+    return self.measure('VRMS', channel)
+
+  def measure_min_voltage(self, channel=1):
+    """
+    Returns the minimum voltage measured.
+    """
+    return self.measure('VMIN', channel)
+
+  def measure_max_voltage(self, channel=1):
+    """
+    Returns the maximum voltage measured.
+    """
+    return self.measure('VMAX', channel)
